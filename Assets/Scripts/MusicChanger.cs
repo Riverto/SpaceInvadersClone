ece4eadcbddb4944ad54e50b0a4a44b8f4b21b21
Unknown <a07025304@itesm.mx>
2018-02-25 21:19:52 -0600

    Designed level Added waves to FormationController Added level specific music

---
+++ Assets/Scripts/MusicChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicChanger : MonoBehaviour {
+    public AudioClip LevelMusic;
+
+
+	// Use this for initialization
+	void Start () {
+        GameObject MPObject = GameObject.Find("Music Player");
+        AudioSource musicPlayer = MPObject.GetComponent<AudioSource>();
+        musicPlayer.Stop();
+        musicPlayer.clip = LevelMusic;
+        musicPlayer.loop = true;
+        musicPlayer.Play();
+	}
+}
